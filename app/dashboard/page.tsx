4d072f07e2fecb6cab502629076586e62659f7e0
Gift <aldreigiftf@gmail.com>
2026-02-07 22:45:14 +0800

    marg

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -494,7 +494,7 @@ const timestamp = new Date().toISOString();
             </div>
           </div>
         </div>
-        <div className="bg-white/95 backdrop-blur-lg rounded-2xl p-8 shadow-xl mt-24">
+        <div className="bg-white/95 backdrop-blur-lg rounded-2xl p-8 shadow-xl mt-4">
         {/* Statistics Section */}
           <Statistics reports={reports} />
         </div>
